f0fe33c864934d5bbfd0ae21d115ab1c0a67cc75
Lennart <zebreus@zebre.us>
2024-09-22 21:23:26 +0000

    Add propertys for the most recent symbol stock and price

--- easyeda2kicad/easyeda/easyeda_api.py
+++ easyeda2kicad/easyeda/easyeda_api.py
@@ -1,11 +1,13 @@
 # Global imports
 import logging
-
+from typing import Optional
 import requests
 
 from easyeda2kicad import __version__
 
 API_ENDPOINT = "https://easyeda.com/api/products/{lcsc_id}/components?version=6.4.19.5"
+LCSC_PRICES_API_ENDPOINT = "https://easyeda.com/api/getPrices?numbers={lcsc_id}&version=6.5.47"
+JLCPCB_STOCK_API_ENDPOINT = "https://easyeda.com/api/components/getSmtPartInfo?version=6.5.47&numbers={lcsc_id}"
 ENDPOINT_3D_MODEL = "https://modules.easyeda.com/3dmodel/{uuid}"
 ENDPOINT_3D_MODEL_STEP = "https://modules.easyeda.com/qAxj6KHrDKw4blvCG8QJPs7Y/{uuid}"
 # ENDPOINT_3D_MODEL_STEP found in https://modules.lceda.cn/smt-gl-engine/0.8.22.6032922c/smt-gl-engine.js : points to the bucket containing the step files.
@@ -38,7 +40,53 @@ class EasyedaApi:
         cp_cad_info = self.get_info_from_easyeda_api(lcsc_id=lcsc_id)
         if cp_cad_info == {}:
             return {}
-        return cp_cad_info["result"]
+        result = cp_cad_info["result"]
+
+        # Add the current lcsc price
+        lcsc_price = self.get_lcsc_price(lcsc_id=lcsc_id)
+        if lcsc_price:
+            result["lcsc_price"] = lcsc_price
+
+        # Add the current jlcpcb stock
+        stock_num = self.get_jlcpcb_stock(lcsc_id=lcsc_id)
+        if stock_num:
+            result["jlc_stock"] = stock_num        
+
+        return result
+    
+    def get_lcsc_price(self, lcsc_id: str) -> Optional[float]:
+        r = requests.get(url=LCSC_PRICES_API_ENDPOINT.format(lcsc_id=lcsc_id), headers=self.headers)
+        api_response = r.json()
+
+        if not api_response or (
+            api_response.get("success", False) is False
+        ):
+            logging.debug(f"{api_response}")
+            return None
+
+        results = api_response.get("result", [])
+        for result in results:
+            price = result.get("lcsc", {}).get("price")
+            if price:
+                return price
+        logging.warning(f"No price available for {lcsc_id} on LCSC")
+        return None
+
+    def get_jlcpcb_stock(self, lcsc_id: str) -> Optional[int]:
+        r = requests.get(url=JLCPCB_STOCK_API_ENDPOINT.format(lcsc_id=lcsc_id), headers=self.headers)
+        api_response = r.json()
+
+        if not api_response or (
+            api_response.get("success", False) is False
+        ):
+            logging.debug(f"{api_response}")
+            return None
+
+        result = api_response.get("result", {})
+        stock = result.get("stock_num", None)
+        if stock == None:
+            logging.debug(f"No SMT service available for {lcsc_id}")
+        return stock
 
     def get_raw_3d_model_obj(self, uuid: str) -> str:
         r = requests.get(
